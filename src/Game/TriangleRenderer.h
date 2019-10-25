c55d2761879cacf64603a79ac1057ef19fa36188
Samuel Walsh (s5068530) <s5068530@bournemouth.ac.uk>
2019-10-25 14:35:38 +0100

    Triangle Rendered to Screen

--- src/Game/TriangleRenderer.h
+++ src/Game/TriangleRenderer.h
@@ -1,7 +1,19 @@
 #include <Eng/Eng.h>
 
+#include <SDL2/SDL.h>
+#include <GL/glew.h>
+
 //Header
 class TriangleRenderer : public Component
 {
+public:
+  TriangleRenderer();
+
   void draw();
+
+private:
+  SDL_Window *window;
+  GLuint programId;
+  GLuint vaoId;
+
 };
