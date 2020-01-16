1b7bc9d4e7142cdf5f974664ba1bf091dce18425
Samuel Walsh (s5068530) <s5068530@bournemouth.ac.uk>
2020-01-16 11:24:46 +0000

    Adding Doxygen

--- src/Eng/Core.h
+++ src/Eng/Core.h
@@ -8,13 +8,33 @@ namespace Eng
 
   class Entity;
 
+  /**
+  * Represents the Core of the Engine
+  * Everything runs through this class including the game loop
+  */
   class Core
   {
   public:
+    /**
+    * \breif Initialize the Engine
+    * By Initializing the engine it creates an SDL Window
+    * By being static it allows this function to be called even if there is no Core object in the game.
+    */
     static std::shared_ptr<Core> initialize();
 
+    /*
+    * \brief Start function 
+    * This function starts the game loop and will us the running boolean to control if the window is to be closed.
+    * It also iterated through all Entity objects in the vector list of Entities and calls tick and display.
+    */
     void start();
+
+    /*
+    * \brief Stop function
+    * This function will change the running bool to false which will interupt and close the game loop in the function start
+    */
     void stop();
+
     std::shared_ptr<Entity> addEntity();
 
     const float m_WindowWidth = 800;
